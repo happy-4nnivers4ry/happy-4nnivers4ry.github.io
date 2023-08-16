abfdd9a2789e036ecee4cff5b7a54d1243488a7e
happy-4nnivers4ry <142434311+happy-4nnivers4ry@users.noreply.github.com>
2023-08-16 23:37:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -47,8 +47,8 @@ function startSnakeGame() {
         if (d == "DOWN") snakeY += box;
 
         // New: Logic for the snake to go through walls
-        if (snakeX < 0) snakeX = canvas.width - box;
-        if (snakeY < 0) snakeY = canvas.height - box;
+        if (snakeX <= 0) snakeX = canvas.width - box;
+        if (snakeY <= 0) snakeY = canvas.height - box;
         if (snakeX >= canvas.width) snakeX = 0;
         if (snakeY >= canvas.height) snakeY = 0;
 
@@ -97,6 +97,104 @@ function startSnakeGame() {
     let game = setInterval(draw, getSpeed());
 }
 
+function startArkanoidGame() {
+    const gameElement = document.getElementById('arkanoidGame');
+    const width = 40;
+    const height = 20;
+    let ball = { x: 20, y: 18, dx: 1, dy: -1 };
+    let paddle = { x: 18, y: 19, dx: 0 };
+    let bricks = [
+        { x: 5, y: 5, type: 'double' },
+        { x: 15, y: 5, type: 'simple' },
+        { x: 25, y: 5, type: 'double' },
+        { x: 10, y: 7, type: 'simple' },
+        { x: 20, y: 7, type: 'simple' },
+        { x: 30, y: 7, type: 'double' },
+        { x: 5, y: 9, type: 'simple' },
+        { x: 15, y: 9, type: 'double' },
+        { x: 25, y: 9, type: 'simple' },
+        { x: 20, y: 11, type: 'double' }
+    ];
+
+    document.addEventListener("keydown", function(event) {
+        if (event.keyCode == 37) paddle.dx = -1;
+        if (event.keyCode == 39) paddle.dx = 1;
+    });
+
+    document.addEventListener("keyup", function(event) {
+        if (event.keyCode == 37 || event.keyCode == 39) paddle.dx = 0;
+    });
+
+    function draw() {
+        let display = Array(height).fill(null).map(row => Array(width).fill(' '));
+        
+        // Draw ball
+        display[ball.y][ball.x] = 'o';
+        
+        // Draw paddle
+        display[paddle.y][paddle.x] = '=';
+        display[paddle.y][paddle.x + 1] = '=';
+        display[paddle.y][paddle.x + 2] = '=';
+
+        // Draw bricks
+        for (let brick of bricks) {
+            display[brick.y][brick.x] = '[';
+            display[brick.y][brick.x + 1] = brick.type === 'double' ? 'X' : ' ';
+            display[brick.y][brick.x + 2] = brick.type === 'double' ? 'X' : ' ';
+            display[brick.y][brick.x + 3] = ']';
+        }
+
+        gameElement.textContent = display.map(row => row.join('')).join('\n');
+    }
+
+    function update() {
+        ball.x += ball.dx;
+        ball.y += ball.dy;
+        paddle.x += paddle.dx;
+
+        // Ball collisions with walls
+        if (ball.x <= 0 || ball.x >= width - 1) ball.dx = -ball.dx;
+        if (ball.y <= 0) ball.dy = -ball.dy;
+
+        // Ball collision with paddle
+        if (ball.y === paddle.y && ball.x >= paddle.x && ball.x <= paddle.x + 2) ball.dy = -ball.dy;
+
+        // Ball collision with bricks
+        for (let i = 0; i < bricks.length; i++) {
+            if (ball.y === bricks[i].y && ball.x >= bricks[i].x && ball.x <= bricks[i].x + 3) {
+                ball.dy = -ball.dy;
+                if (bricks[i].type === 'double') {
+                    bricks[i].type = 'simple';
+                } else {
+                    bricks.splice(i, 1);
+                }
+                break;
+            }
+        }
+
+        // Game over condition
+        if (ball.y >= height - 1) {
+            clearInterval(gameInterval);
+            startArkanoidGame();
+        }
+
+        // Winning condition
+        if (bricks.length === 0) {
+            clearInterval(gameInterval);
+            gameElement.style.display = 'none';
+            document.getElementById("outputMessage").innerText = "ANSWER3";
+        }
+    }
+
+    function gameLoop() {
+        update();
+        draw();
+    }
+
+    const gameInterval = setInterval(gameLoop, 100);
+}
+
+
 
 function checkSecretCode() {
     const userInput = document.getElementById("secretCodeInput").value.trim().toUpperCase();
@@ -117,7 +215,8 @@ function checkSecretCode() {
             startSnakeGame();
             break;
         case "PASSWORD0":
-            message = "ANSWER3";
+            document.getElementById('arkanoidGame').style.display = 'block';
+            startArkanoidGame();
             break;
         default:
             message = randomResponses[Math.floor(Math.random() * randomResponses.length)];
